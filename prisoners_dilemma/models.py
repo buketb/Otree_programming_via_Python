6ea28b3d48634548c542cc52dc08d1d71c6b531f
buketb <30777574+buketb@users.noreply.github.com>
2017-08-09 11:45:00 +0200

    prisoners_dilemma_second

--- prisoners_dilemma/models.py
+++ prisoners_dilemma/models.py
@@ -30,20 +30,27 @@ class Group(BaseGroup):
         if player1.decision == "stay silent" and player2.decision == "stay silent": 
             player1.years_in_prison = 1
             player2.years_in_prison = 1
+            player1.others_decision = "stay silent"
+            player2.others_decision = "stay silent"
+
 
         if player1.decision == "stay silent" and player2.decision == "betray":
             player1.years_in_prison = 3
             player2.years_in_prison = 0
+            player1.others_decision = "stay silent"
+            player2.others_decision = "betray"
 
         if player1.decision == "betray" and player2.decision == "stay silent":
             player1.years_in_prison = 0
             player2.years_in_prison = 3
+            player1.others_decision = "betray"
+            player2.others_decision = "stay silent"
 
         if player1.decision == "betray" and player2.decision == "betray":
             player1.years_in_prison = 2
             player2.years_in_prison = 2
-
-
+            player1.others_decision = "betray"
+            player2.others_decision = "betray"
 
 
 class Player(BasePlayer):
@@ -55,3 +62,8 @@ class Player(BasePlayer):
     years_in_prison = models.PositiveIntegerField(
         verbose_name= "How many years will you stay in prison",
         doc= "the data of the years in prison")
+
+    others_decision = models.CharField( choices=["stay silent", "betray"],
+        verbose_name= "What is the other's choice?",
+        doc= "the data of the other's choice"
+        )
